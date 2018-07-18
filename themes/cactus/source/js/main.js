b1b44f64018cad46b6c77b175f920fd30d4f7950
kaka <kanniu@163.com>
2018-07-18 20:44:08 +0800

    增加 捐赠 模块

--- themes/cactus/source/js/main.js
+++ themes/cactus/source/js/main.js
@@ -12,6 +12,45 @@ if (!!$.prototype.justifiedGallery) {
 
 
 $(document).ready(function() {
+  // donate start
+  var QRBox	=	$('#QRBox');
+  var MainBox	=	$('.MainBox');
+  var BTCQR	=	$('#BTCQR');
+  var AliPayQR	=	$('#AliPayQR');
+  var WeChatQR	=	$('#WeChatQR');
+  var currentQR;
+
+  function showQR(QR) {
+    $('#DonateText,#donateBox,#github').addClass('blur');
+    currentQR = QR;
+    QRBox.fadeIn(300,function(argument) {
+      QR.addClass('showQR');
+    });
+  }
+
+  $('#donateBox>li').click(function(event) {
+    var thisID	=	$(this).attr('id');
+    if (thisID === 'BTC') {
+      showQR(BTCQR);
+      new Clipboard('#BTCBn');
+    } else if (thisID === 'AliPay') {
+      showQR(AliPayQR);
+    } else if (thisID === 'WeChat') {
+      showQR(WeChatQR);
+    }
+  });
+
+  MainBox.click(function(event) {
+    if (currentQR) currentQR.removeClass('showQR').addClass('hideQR');
+    setTimeout (function(a) {
+      QRBox.fadeOut(300,function(argument) {
+        MainBox.removeClass('hideQR');
+      });
+      $('#DonateText,#donateBox,#github').removeClass('blur');
+    },600);
+
+  });
+  // donate end
 
   /**
    * Shows the responsive navigation menu on mobile.
