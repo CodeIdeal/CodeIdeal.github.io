89068c2bf1ced6f67d681d9df551e64ce906a581
CodeIdeal <CodeIdeal@users.noreply.github.com>
2026-01-15 11:12:16 +0000

    deploy: cbae70be73485a6b319b8d52328db0e32b1a79fb

--- js/main.js
+++ js/main.js
@@ -10,44 +10,40 @@ if (!!$.prototype.justifiedGallery) {
   $(".article-gallery").justifiedGallery(options);
 }
 
-
-$(document).ready(function() {
+$(document).ready(function () {
   // donate start
-  var QRBox	=	$('#QRBox');
-  var MainBox	=	$('.MainBox');
-  var BTCQR	=	$('#BTCQR');
-  var AliPayQR	=	$('#AliPayQR');
-  var WeChatQR	=	$('#WeChatQR');
+  var QRBox = $('#QRBox');
+  var MainBox = $('.MainBox');
+  var BTCQR = $('#BTCQR');
+  var AliPayQR = $('#AliPayQR');
+  var WeChatQR = $('#WeChatQR');
   var currentQR;
 
   function showQR(QR) {
     $('#DonateText,#donateBox,#github').addClass('blur');
     currentQR = QR;
-    QRBox.fadeIn(300,function(argument) {
+    QRBox.fadeIn(300, function (argument) {
       QR.addClass('showQR');
     });
   }
 
-  $('#donateBox>li').click(function(event) {
-    var thisID	=	$(this).attr('id');
-    if (thisID === 'BTC') {
-      showQR(BTCQR);
-      new Clipboard('#BTCBn');
-    } else if (thisID === 'AliPay') {
+  $('#donateBox>li').click(function (event) {
+    var thisID = $(this).attr('id');
+    if (thisID === 'AliPay') {
       showQR(AliPayQR);
     } else if (thisID === 'WeChat') {
       showQR(WeChatQR);
     }
   });
 
-  MainBox.click(function(event) {
+  MainBox.click(function (event) {
     if (currentQR) currentQR.removeClass('showQR').addClass('hideQR');
-    setTimeout (function(a) {
-      QRBox.fadeOut(300,function(argument) {
+    setTimeout(function (a) {
+      QRBox.fadeOut(300, function (argument) {
         MainBox.removeClass('hideQR');
       });
       $('#DonateText,#donateBox,#github').removeClass('blur');
-    },600);
+    }, 600);
 
   });
   // donate end
@@ -55,7 +51,7 @@ $(document).ready(function() {
   /**
    * Shows the responsive navigation menu on mobile.
    */
-  $("#header > #nav > ul > .icon").click(function() {
+  $("#header > #nav > ul > .icon").click(function () {
     $("#header > #nav > ul").toggleClass("responsive");
   });
 
@@ -73,19 +69,19 @@ $(document).ready(function() {
      * Display the menu on hi-res laptops and desktops.
      */
     if ($(document).width() >= 1440) {
-      menu.css("visibility", "visible");
+      menu.show();
       menuIcon.addClass("active");
     }
 
     /**
      * Display the menu if the menu icon is clicked.
      */
-    menuIcon.click(function() {
-      if (menu.css("visibility") === "hidden") {
-        menu.css("visibility", "visible");
+    menuIcon.click(function () {
+      if (menu.is(":hidden")) {
+        menu.show();
         menuIcon.addClass("active");
       } else {
-        menu.css("visibility", "hidden");
+        menu.hide();
         menuIcon.removeClass("active");
       }
       return false;
@@ -95,7 +91,7 @@ $(document).ready(function() {
      * Add a scroll listener to the menu to hide/show the navigation links.
      */
     if (menu.length) {
-      $(window).on("scroll", function() {
+      $(window).on("scroll", function () {
         var topDistance = menu.offset().top;
 
         // hide only the navigation links on desktop
@@ -107,10 +103,10 @@ $(document).ready(function() {
 
         // on tablet, hide the navigation icon as well and show a "scroll to top
         // icon" instead
-        if ( ! $( "#menu-icon" ).is(":visible") && topDistance < 50 ) {
+        if (!$("#menu-icon").is(":visible") && topDistance < 50) {
           $("#menu-icon-tablet").show();
           $("#top-icon-tablet").hide();
-        } else if (! $( "#menu-icon" ).is(":visible") && topDistance > 100) {
+        } else if (!$("#menu-icon").is(":visible") && topDistance > 100) {
           $("#menu-icon-tablet").hide();
           $("#top-icon-tablet").show();
         }
@@ -121,12 +117,12 @@ $(document).ready(function() {
      * Show mobile navigation menu after scrolling upwards,
      * hide it again after scrolling downwards.
      */
-    if ($( "#footer-post").length) {
+    if ($("#footer-post").length) {
       var lastScrollTop = 0;
-      $(window).on("scroll", function() {
+      $(window).on("scroll", function () {
         var topDistance = $(window).scrollTop();
 
-        if (topDistance > lastScrollTop){
+        if (topDistance > lastScrollTop) {
           // downscroll -> show menu
           $("#footer-post").hide();
         } else {
